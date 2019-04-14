0843b99a3a3591d90410b801fee0dbfb0474f0ef
Ruth Newman <ruthnewman@ruths-air.home>
2019-04-14 20:12:09 +0100

    added sound effects and glow

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const avocadoDiv = document.getElementById('avocado');
 const beerDiv = document.getElementById('beer');
 const latteDiv = document.getElementById('latte');
 
+const win = new Audio();
+win.src = "../sounds/alert1.wav";
+const draw = new Audio();
+draw.src = "../sounds/alert2.wav";
+const lose = new Audio();
+lose.src = "../sounds/failure1.wav";
+
 function getComputerChoice() {
   const choices = ["avocado", "beer", "latte"];
   const randomNumber = (Math.floor(Math.random() * 3));
@@ -30,6 +37,8 @@ function userWins(user, computer) {
   userScoreSpan.innerHTML = userScore;
   computerScoreSpan.innerHTML = computerScore;
   resultDiv.innerHTML = `${formatResult(user)} defeats ${formatSecondResult(computer)}. You win!`;
+  win.play()
+  document.getElementById(user).classList = 'choice greenGlow';
 }
 
 function userLoses(user, computer) {
@@ -37,6 +46,8 @@ function userLoses(user, computer) {
   computerScoreSpan.innerHTML = computerScore;
   userScoreSpan.innerHTML = userScore;
   resultDiv.innerHTML = `${formatResult(computer)} defeats ${formatSecondResult(user)}. You lose!`;
+  lose.play();
+  document.getElementById(user).classList = 'choice redGlow';
 }
 
 function itsADraw(user, computer) {
@@ -45,6 +56,8 @@ function itsADraw(user, computer) {
   computerScoreSpan.innerHTML = computerScore;
   userScoreSpan.innerHTML = userScore;
   resultDiv.innerHTML = `The virtual hipster also chose ${formatSecondResult(computer)}. It's a draw. Play again!`;
+  draw.play();
+  document.getElementById(user).classList = 'choice yellowGlow';
 }
 
 function game(userChoice) {
